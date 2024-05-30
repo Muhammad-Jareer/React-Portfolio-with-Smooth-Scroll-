e8449dd98fedfb99c51933957026da08e8d4661d
Muhammad Jareer <jareerameer@gmail.com>
2024-05-30 14:49:57 +0500

    Heading fixed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         >
             <div className='py-4 max-w-screen-lg h-full mx-auto flex flex-col items-center justify-center gap-8 md:gap-16 px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
-                    <h2 className='text-4xl md:text-7xl font-bold text-white pt-6'>I'm <span className="bg-gradient-to-t from-cyan-200 to-blue-500 text-transparent bg-clip-text font-signature">Muhammad Jareer</span> Front-end Developer</h2>
+                    <h2 className='text-4xl md:text-7xl font-bold text-white pt-6'>I'm <span className="bg-gradient-to-t from-cyan-200 to-blue-500 text-transparent bg-clip-text">Muhammad Jareer</span> Front-end Developer</h2>
                     <p className='text-gray-500 py-4 text-lg font-bold'>Skilled web developer with expertise in HTML, CSS, Tailwind, JavaScript, GSAP, React, and GitHub. Focused on building dynamic and responsive web applications.</p>
 
 
